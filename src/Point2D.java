72e36e56542aea44c6e82abda8970bd561cb30b6
Sara Al-Jammal <sara.aljammal97@gmail.com>
2020-04-23 20:57:10 +0300

    Starting the project

---
+++ src/Point2D.java
@@ -0,0 +1,25 @@
+
+/*
+ * This is my DevOps homework
+ * github repository already was created.
+ *
+ * I will start with this class and upload it on github on branch master
+ *
+ * I will make another branch and more classes
+ * */
+
+import java.util.*;
+        import java.lang.*;
+
+public class Point2D{
+
+    public Point2D(){
+        System.out.println("This is a 2D point");
+    }
+    public static void main (String[] args) throws java.lang.Exception {
+
+        Point2D p2 = new Point2D();
+        //Point3D p3 = new Point3D();
+        System.out.println("This is the main function");
+    }
+}
